ddaa9156e7dcdf1bbb88d0db9a03c4ab3e3ae2b8
India Rex <india.rex@and.digital>
2021-07-30 10:20:59 +0100

    Update app test to work with default values

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,13 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-const mockTodos = [
-  {title: "here's one"},
-  {title: 'and another'}
-]
-
 test('renders learn react link', () => {
-  render(<App todos={mockTodos}/>);
-  const linkElement = screen.getByText(/and another/i);
+  render(<App />);
+  const linkElement = screen.getByText(/another/i);
   expect(linkElement).toBeInTheDocument();
 });
